dca7a234a17c1fd47a8fbfca858cc1d4770c2521
hackerhihello <hackerhihello123@gmail.com>
2024-08-04 21:36:23 +0530

    hsjds

--- scripts.js
+++ scripts.js
@@ -1,19 +1,18 @@
 // Debugging to ensure script is loaded
-console.log("Script loaded");
+// console.log("Script loaded");
 
 function showWish() {
-    console.log("showWish function called");
+    // console.log("showWish function called");
     document.getElementById('popup').style.display = 'block';
     document.getElementById('videoFrame').src = 'https://www.youtube.com/embed/ePjLGie443U';
 }
 
 function closePopup() {
-    console.log("closePopup function called");
     document.getElementById('popup').style.display = 'none';
     document.getElementById('videoFrame').src = ''; // Stop the video when the popup is closed
 }
 
 // Ensure the script is correctly attaching the event listener
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOM fully loaded and parsed");
+    // console.log("DOM fully loaded and parsed");
 });
